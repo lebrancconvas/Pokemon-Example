f9a35df3c6e8590152d7943343dee1f975ce00b1
lebrancconvas <lebrancconvas@gmail.com>
2022-07-27 21:52:26 +0700

    ✨ feat: complete script to fetch the api data

--- web/main.js
+++ web/main.js
@@ -1,22 +1,31 @@
 import './style.css'
 
-let pokemonElement = ``;
-
-const pokemonSection = document.getElementById('pokemon');
+let pokemonSection = document.getElementById('pokemon');
 
 const getPokemon = async() => {
-  const res = await fetch('http://localhost:9001/pokemons');
-  const data = await res.json();
-  return data;
+  try {
+    const record = undefined;
+    const res = await fetch(`http://localhost:5002/api/v1/pokemons`, {headers: {'Content-Type': 'application/json'}});
+    const data = await res.json();
+    return data;
+  } catch(err) {
+    console.error(err);
+    console.log('[FAILURE] Fetch Data Failed.'); 
+  }
 };
 
 getPokemon().then(pokemons => {
   pokemons.map(pokemon => {
-    pokemonElement += `<div id="pokemon-card"><div id="pokemon-thumbnail"><img src="${pokemon.ThumbnailImage}" alt="${pokemon.name}"></div><div id="pokemon-details"><div id="pokemon-name">${pokemon.name}</div><div id="pokemon-type">${pokemon.type}</div></div></div>`;
+    let pokemonElement = `<div id="pokemon-card"><div id="pokemon-thumbnail"><img src="${pokemon.ThumbnailImage}" alt="${pokemon.name}" /></div><div id="pokemon-details"><div id="pokemon-name">${pokemon.name}</div><div id="pokemon-type">${pokemon.type}</div></div></div>\n`;
+    pokemonSection.insertAdjacentHTML('beforeend', pokemonElement);
+    console.log(`${pokemonElement}`); // Debugging. 
   })
-});
+}).catch(err => {
+  console.error(err);
+  console.log('[FAILURE] Get Pokemon Element Failed.'); 
+})
 
-pokemonSection.insertAdjacentHTML('beforeend', pokemonElement); 
+
 
 
 
